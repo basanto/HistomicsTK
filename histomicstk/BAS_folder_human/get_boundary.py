a5741af06ed44f6533db26210eaa683ca493edfd
BrianaSanto <48836173+basanto@users.noreply.github.com>
2021-02-02 19:19:15 -0500

    Update get_boundary.py

--- histomicstk/BAS_folder_human/get_boundary.py
+++ histomicstk/BAS_folder_human/get_boundary.py
@@ -7,7 +7,7 @@ import numpy as np
 import cv2
 
 def get_boundary(image):
-    _,boundary_pts,_ = cv2.findContours(np.uint8(image), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    boundary_pts,_ = cv2.findContours(np.uint8(image), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     boundary_pts = np.vstack(boundary_pts)
     boundary_pts = boundary_pts.reshape(len(boundary_pts),2)
     return boundary_pts
